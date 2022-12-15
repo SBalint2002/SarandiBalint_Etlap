b987ecb629d12259d496c6f81385b2f072f28430
Sárándi <sarandi.balint@gmail.com>
2022-12-15 14:59:55 +0100

    Etlap class

--- src/main/java/com/etlap/etlap/App.java
+++ src/main/java/com/etlap/etlap/App.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("etlap-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("etlap-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Étlap");
         stage.setScene(scene);
         stage.show();
     }
